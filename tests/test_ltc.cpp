737c7c7a0696cc599f8d3e152a05973fd5600b54
Martin <>
2018-11-29 13:19:57 -0400

    Write new test for LTC, but they are not perfect.

--- tests/test_ltc.cpp
+++ tests/test_ltc.cpp
@@ -1,3 +1,5 @@
+#include <vector>
+#include <cmath>
 #include "gtest/gtest.h"
 #include "ltc.hpp"
 
@@ -17,4 +19,56 @@ TEST(LTC, Basic) {
 	comp.get_value_to_transmit(timestamp, value);
 	EXPECT_EQ (0, timestamp);
 	EXPECT_EQ (first_value, value);
+}
+TEST(LTC, Perfect) { 
+	LTC<3> comp;
+	int count_compress = 0;
+	for(int i = 0; i < 400; ++i){
+		bool a = comp.add(i, i);
+		if(a)
+			count_compress += 1;
+	}
+	EXPECT_EQ (0, count_compress); //It is one line only, so ltc should never break the line
+}
+TEST(LTC, Linear) {
+	LTC<3> comp;
+	int count_compress = 0;
+	for(int i = 0; i < 1100; ++i){
+		bool a = comp.add(i, i%200);
+		if(a)
+			count_compress += 1;
+	}
+	EXPECT_TRUE (count_compress < 30); //It is very linear, so I expect good compression
+	EXPECT_TRUE (count_compress > 5); //There is 5 lines (1100 / 200)
+}
+TEST(LTC, Cos) { 
+	LTC<3> comp;
+	int count_compress = 0;
+	for(int i = 0; i < 1100; ++i){
+		int val = round(cos(i*0.1) * 20);
+		bool a = comp.add(i, val);
+		if(a)
+			count_compress += 1;
+	}
+	EXPECT_TRUE (count_compress < 1100); //Some compression is expected
+	EXPECT_TRUE (count_compress > 4); //More than 4 because there is roughly 4 linear lines in this function
+}
+TEST(LTC, Compression_level) { 
+	LTC<3> comp3;
+	LTC<5> comp5;
+	int count_3 = 0, count_5 = 0;
+	for(int i = 0; i < 1100; ++i){
+		int val = round(cos(i*0.1) * 20);
+		bool a = comp3.add(i, val);
+		if(a)
+			count_3 += 1;
+		a = comp5.add(i, val);
+		if(a)
+			count_5 += 1;
+	}
+	EXPECT_TRUE (count_3 < 1100); //Some compression is expected
+	EXPECT_TRUE (count_3 > 4); //More than 4 because there is roughly 4 linear lines in this function
+	EXPECT_TRUE (count_5 < 1100); //Some compression is expected
+	EXPECT_TRUE (count_5 > 4); //More than 4 because there is roughly 4 linear lines in this function
+	EXPECT_TRUE (count_5 < count_3); //comp5 has a better compression level
 }
