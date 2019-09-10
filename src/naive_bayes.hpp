402fa785dfcdad58dea89c689c0bec2c512d01c6
Martin <martin.khannouz@gmail.com>
2019-09-10 17:21:31 -0400

    Add generic methods for train and predict in naive bayes so the generic methods turn the features into int array.

--- src/naive_bayes.hpp
+++ src/naive_bayes.hpp
@@ -23,6 +23,81 @@ class NaiveBayes{
 	int* counters_label;
 	//The smoothing value
 	double smoothing;
+
+		/*
+		* Predict the label of the given data point.
+		* @param features an array of size *feature_count* that contains the value of the data point.
+		* @param scores a pointer of size *label_count* that will store the score for each label.
+		*/
+	int _predict(int const* features, double* scores = nullptr){
+		//Compute the total count of data points
+		double sum_datapoints = 0;
+		for(int label = 0; label < label_count; ++label)
+			sum_datapoints += counters_label[label];
+
+		//Declare the score for each label
+		double label_scores[label_count];
+
+		for(int label = 0; label < label_count; ++label){
+			//Initialize the score for the label with the prior probability
+			double const label_numerator = static_cast<double>(counters_label[label]) + static_cast<double>(sum_feature_size)*smoothing;
+			label_scores[label] = func::log(label_numerator / sum_datapoints);
+			//Set the starting index for the counters for the current label
+			int const label_idx = label * sum_feature_size;
+			//feature_idx stores the starting index for the counters of a specific feature (each feature has a counter for each of its values)
+			int feature_idx = 0;
+			for(int feature = 0; feature < feature_count; ++feature){
+				//The sum of all the counter for this feature
+				double sum_current_feature = static_cast<double>(features_size[feature]) * smoothing;
+				//Get the value of the counter for the value of the current feature
+				double const current_feature_count = counters[label_idx + feature_idx + features[feature]] + smoothing;
+				//Compute the sum for the current feature
+				for(int feature_value = 0; feature_value < features_size[feature]; ++feature_value){
+					int const idx = label_idx + feature_idx + feature_value;
+					sum_current_feature += counters[idx];
+				}
+				//Add the score for the current feature. Use a log function to avoid underflow
+				label_scores[label] += func::log(current_feature_count / sum_current_feature);
+				//Increase *feature_idx* to the index of the next feature
+				feature_idx += features_size[feature];
+			}
+		}
+
+		//Get the best label
+		int best_score_idx = 0;
+		for(int label = 1; label < label_count; ++label)
+			if(label_scores[label] > label_scores[best_score_idx])
+				best_score_idx = label;
+
+		//If the label scores are needed, we copy them
+		if(scores != nullptr)
+			for(int label = 0; label < label_count; ++label)
+				scores[label] = label_scores[label];
+		return best_score_idx;
+	}
+		/*
+		* Train the model with one data point.
+		* @param features an array of size *feature_count* that contain how many value there is for each feature.
+		* @param smoothing the smoothing value to use.
+		*/
+	bool _train(int const* features, int const label){
+		//Find the first index for the counter of this label
+		int counter_idx = label * sum_feature_size;
+		//For each feature, increase the counter corresponding to the value
+		for(int i = 0; i < feature_count; ++i){
+			if(features[i] >= features_size[i])
+				return false;
+			//Calculate the index of the counter
+			int const tmp_idx = counter_idx + features[i];
+			//Increase the counter
+			counters[tmp_idx] += 1;
+			//Increase to the next feature
+			counter_idx += features_size[i];
+		}
+		//Increase the count of occurence for *label*
+		counters_label[label] += 1;
+		return true;
+	}
 	public:
 		/*
 		* Constructor.
@@ -38,7 +113,7 @@ class NaiveBayes{
 				sum_feature_size += features_size[i];
 			}
 			//Allocate and initialize the counters
-			counters = (int*)func::malloc(label_count*sum_feature_size*sizeof(int));
+			counters = static_cast<int*>(func::malloc(label_count*sum_feature_size*sizeof(int)));
 			for(int i = 0; i < label_count*sum_feature_size; ++i)
 				counters[i] = 0;
 
@@ -52,74 +127,42 @@ class NaiveBayes{
 		* @param features an array of size *feature_count* that contain how many value there is for each feature.
 		* @param smoothing the smoothing value to use.
 		*/
+		template<class T>
+		int train(T* features, int const label){
+			int alt_features[feature_count];
+			for(int i = 0; i < feature_count; ++i)
+				alt_features[i] = static_cast<int>(features[i]);
+			return _train(alt_features, label);
+		}
+		/*
+		* Train the model with one data point.
+		* @param features an array of size *feature_count* that contain how many value there is for each feature.
+		* @param smoothing the smoothing value to use.
+		*/
+		template<int>
 		bool train(int const* features, int const label){
-			//Find the first index for the counter of this label
-			int counter_idx = label * sum_feature_size;
-			//For each feature, increase the counter corresponding to the value
-			for(int i = 0; i < feature_count; ++i){
-				if(features[i] >= features_size[i])
-					return false;
-				//Calculate the index of the counter
-				int const tmp_idx = counter_idx + features[i];
-				//Increase the counter
-				counters[tmp_idx] += 1;
-				//Increase to the next feature
-				counter_idx += features_size[i];
-			}
-			//Increase the count of occurence for *label*
-			counters_label[label] += 1;
-			return true;
+			return _train(features, label);
 		}
 		/*
 		* Predict the label of the given data point.
 		* @param features an array of size *feature_count* that contains the value of the data point.
 		* @param scores a pointer of size *label_count* that will store the score for each label.
 		*/
+		template<class T>
+		int predict(T* features, double* scores = nullptr){
+			int alt_features[feature_count];
+			for(int i = 0; i < feature_count; ++i)
+				alt_features[i] = static_cast<int>(features[i]);
+			return _predict(alt_features, scores);
+		}
+		/*
+		* Predict the label of the given data point.
+		* @param features an array of size *feature_count* that contains the value of the data point.
+		* @param scores a pointer of size *label_count* that will store the score for each label.
+		*/
+		template<int>
 		int predict(int const* features, double* scores = nullptr){
-			//Compute the total count of data points
-			double sum_datapoints = 0;
-			for(int label = 0; label < label_count; ++label)
-				sum_datapoints += counters_label[label];
-
-			//Declare the score for each label
-			double label_scores[label_count];
-
-			for(int label = 0; label < label_count; ++label){
-				//Initialize the score for the label with the prior probability
-				double const label_numerator = static_cast<double>(counters_label[label]) + static_cast<double>(sum_feature_size)*smoothing;
-				label_scores[label] = func::log(label_numerator / sum_datapoints);
-				//Set the starting index for the counters for the current label
-				int const label_idx = label * sum_feature_size;
-				//feature_idx stores the starting index for the counters of a specific feature (each feature has a counter for each of its values)
-				int feature_idx = 0;
-				for(int feature = 0; feature < feature_count; ++feature){
-					//The sum of all the counter for this feature
-					double sum_current_feature = static_cast<double>(features_size[feature]) * smoothing;
-					//Get the value of the counter for the value of the current feature
-					double const current_feature_count = counters[label_idx + feature_idx + features[feature]] + smoothing;
-					//Compute the sum for the current feature
-					for(int feature_value = 0; feature_value < features_size[feature]; ++feature_value){
-						int const idx = label_idx + feature_idx + feature_value;
-						sum_current_feature += counters[idx];
-					}
-					//Add the score for the current feature. Use a log function to avoid underflow
-					label_scores[label] += func::log(current_feature_count / sum_current_feature);
-					//Increase *feature_idx* to the index of the next feature
-					feature_idx += features_size[feature];
-				}
-			}
-
-			//Get the best label
-			int best_score_idx = 0;
-			for(int label = 1; label < label_count; ++label)
-				if(label_scores[label] > label_scores[best_score_idx])
-					best_score_idx = label;
-
-			//If the label scores are needed, we copy them
-			if(scores != nullptr)
-				for(int label = 0; label < label_count; ++label)
-					scores[label] = label_scores[label];
-			return best_score_idx;
+			return _predict(features, scores);
 		}
 		/*
 		 * Destructor
